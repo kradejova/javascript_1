5dc3da3a587ebf9f2ff81216a218b7dd2a5c2b44
kaja <k.radejova@gmail.com>
2024-04-20 09:17:40 +0200

    upraveny vystup

--- domaci_ukoly/DU1_fahrnheit_vs_celsius/script.js
+++ domaci_ukoly/DU1_fahrnheit_vs_celsius/script.js
@@ -3,4 +3,6 @@
 const stupneFahrnheit = Number(prompt("Zadejte pocet stupnu Fahrnheita: "))
 const stupneCelsius = (5 * (stupneFahrnheit - 32) / 9).toFixed(2)
 
-document.body.innerHTML = "<p>" + stupneFahrnheit + " stupnu Fahrnheita se rovna " + stupneCelsius + " stupnum Celsia."
+//document.body.innerHTML = "<p>" + stupneFahrnheit + " stupnu Fahrnheita se rovna " + stupneCelsius + " stupnum Celsia."
+document.body.innerHTML = "<p><b>Fahrnheit: </b>" +  stupneFahrnheit + " °F</p>"
+document.body.innerHTML += "<p><b>Celsius: </b>" + stupneCelsius + " °C"
