5574c219e90c4e06baa72fab2dcacb46a1a69961
kaja <k.radejova@gmail.com>
2024-04-27 23:29:36 +0200

    DU2 - diakritika, metoda replace

--- domaci_ukoly/DU2_fit_mail/script.js
+++ domaci_ukoly/DU2_fit_mail/script.js
@@ -1,6 +1,6 @@
 //jmeno a prijmeni uzivatele, osekana o white spaces a prevedene na mala pismena
-const krestniJmeno = prompt("Zadejte krestni jmeno").trim().toLowerCase()
-const prijmeni = prompt("Zadejte primeni").trim().toLowerCase()
+const krestniJmeno = prompt("Zadejte krestni jmeno").trim().toLowerCase().replace("ě", "e").replace("š","s").replace("č","c").replace("ř","r").replace("ž","z").replace("ý","y").replace("á","a").replace("í","i").replace("é","e").replace("ú","u").replace("ů","u").replace("ť","t").replace("ď","d").replace("ň","n")
+const prijmeni = prompt("Zadejte primeni").trim().toLowerCase().replace("ě", "e").replace("š","s").replace("č","c").replace("ř","r").replace("ž","z").replace("ý","y").replace("á","a").replace("í","i").replace("é","e").replace("ú","u").replace("ů","u").replace("ť","t").replace("ď","d").replace("ň","n")
 
 //cast jmena, cast prijmeni, zbytek emailu
 const castJmena = krestniJmeno.slice(0,3)
