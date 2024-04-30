6a7991a0814bd4db987517579f58870a6323521c
kaja <k.radejova@gmail.com>
2024-04-30 14:24:42 +0200

    DU2 - upraveny kod

--- domaci_ukoly/DU2_fit_mail/script.js
+++ domaci_ukoly/DU2_fit_mail/script.js
@@ -1,6 +1,7 @@
 //jmeno a prijmeni uzivatele, osekana o white spaces a prevedene na mala pismena
-const krestniJmeno = prompt("Zadejte krestni jmeno").trim().toLowerCase().replace("ě", "e").replace("š","s").replace("č","c").replace("ř","r").replace("ž","z").replace("ý","y").replace("á","a").replace("í","i").replace("é","e").replace("ú","u").replace("ů","u").replace("ť","t").replace("ď","d").replace("ň","n")
-const prijmeni = prompt("Zadejte primeni").trim().toLowerCase().replace("ě", "e").replace("š","s").replace("č","c").replace("ř","r").replace("ž","z").replace("ý","y").replace("á","a").replace("í","i").replace("é","e").replace("ú","u").replace("ů","u").replace("ť","t").replace("ď","d").replace("ň","n")
+const krestniJmeno = prompt("Zadejte krestni jmeno (bez diakritiky):").trim().toLowerCase()
+const prijmeni = prompt("Zadejte primeni (bez diakritiky:").trim().toLowerCase()
+
 
 //cast jmena, cast prijmeni, zbytek emailu
 const castJmena = krestniJmeno.slice(0,3)
